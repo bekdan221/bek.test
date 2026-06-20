9767ff07f2ed4ac04b7c316c7645b2f0d6e86138
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 20:26:18 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+function render(list){
+const c=document.getElementById("catalog");
+c.innerHTML="";
 
+list.forEach(car=>{
+c.innerHTML+=`
+<div class="card" onclick="openCar('${car.id}')">
+<img src="${car.img}">
+<h3>${car.name}</h3>
+<p>$${car.price}</p>
+</div>
+`;
+});
+}
+
+render(cars);
+
+function openCar(id){
+location.href="car.html?id="+id;
+}
