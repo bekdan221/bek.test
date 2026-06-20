73bb388cca20fa49d3aef570a71cc9112fccb667
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 19:51:14 +0600

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,1 +1,25 @@
+const cars = [
+  {id:"bmw-m5",brand:"BMW",name:"BMW M5",price:"$65,000",mileage:"58,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/6/6b/2019_BMW_M5.jpg"]},
 
+  {id:"bmw-x5",brand:"BMW",name:"BMW X5",price:"$80,000",mileage:"40,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/0/0c/BMW_X5.jpg"]},
+
+  {id:"audi-rs6",brand:"Audi",name:"Audi RS6",price:"$90,000",mileage:"35,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/4/4e/Audi_RS6.jpg"]},
+
+  {id:"merc-e63",brand:"Mercedes",name:"Mercedes E63 AMG",price:"$88,000",mileage:"50,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/3/3f/Mercedes_AMG_E63.jpg"]},
+
+  {id:"toyota-camry",brand:"Toyota",name:"Toyota Camry",price:"$28,000",mileage:"42,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/9/9b/Toyota_Camry.jpg"]},
+
+  {id:"lexus-lx",brand:"Lexus",name:"Lexus LX 600",price:"$95,000",mileage:"18,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/3/3f/Lexus_LX.jpg"]},
+
+  {id:"porsche-911",brand:"Porsche",name:"Porsche 911",price:"$180,000",mileage:"12,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/7/7e/Porsche_911.jpg"]},
+
+  {id:"range-rover",brand:"Range Rover",name:"Range Rover Sport",price:"$110,000",mileage:"25,000 km",
+  images:["https://upload.wikimedia.org/wikipedia/commons/1/1b/Range_Rover.jpg"]}
+];
