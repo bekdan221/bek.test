ad51e1e65ac1e2ce8e3a40d910e4e89ea3992ff6
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 19:59:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
 function render(list){
-  const c = document.getElementById("catalog");
+  const c=document.getElementById("catalog");
   c.innerHTML="";
 
   list.forEach(car=>{
-    c.innerHTML += `
-      <div class="card">
-        <img src="${car.images[0]}">
+    c.innerHTML+=`
+      <div class="card" onclick="openCar('${car.id}')">
+        <img src="${car.img}">
         <h3>${car.name}</h3>
         <p>${car.price}</p>
-        <button onclick="openCar('${car.id}')">Подробнее</button>
       </div>
     `;
   });
 }
 
-if(document.getElementById("catalog")){
-  render(cars);
-
-  document.getElementById("search").oninput=function(){
-    render(cars.filter(c=>c.name.toLowerCase().includes(this.value.toLowerCase())));
-  }
-
-  document.getElementById("brand").onchange=function(){
-    if(this.value==="all") render(cars);
-    else render(cars.filter(c=>c.brand===this.value));
-  }
-
-  window.openCar=function(id){
-    location.href="car.html?id="+id;
-  }
+function show(brand){
+  if(brand==="ALL") render(cars);
+  else render(cars.filter(c=>c.brand===brand));
 }
 
-if(document.getElementById("title")){
-  const id=new URLSearchParams(location.search).get("id");
-  const car=cars.find(c=>c.id===id);
-
-  document.getElementById("title").innerText=car.name;
-  document.getElementById("mainImg").src=car.images[0];
-
-  document.getElementById("info").innerHTML=`
-    💰 ${car.price}<br>
-    📍 ${car.mileage}
-  `;
-
-  car.images.forEach(img=>{
-    document.getElementById("gallery").innerHTML+=
-    `<img src="${img}" onclick="mainImg.src='${img}'">`;
-  });
+function openCar(id){
+  location.href="car.html?id="+id;
 }
+
+render(cars);
