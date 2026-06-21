5f3360ec2d1cde5fb763a3ad992ae61f11549c62
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-21 17:42:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+function startApp() {
+    document.getElementById("titleScreen").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
 
+function startQuiz(id) {
+    document.getElementById("q").innerText = "Выбран тест: " + id;
+
+    document.getElementById("answers").innerHTML = `
+        <button>Вопрос 1 (пример)</button>
+        <button>Вопрос 2 (пример)</button>
+        <button>Вопрос 3 (пример)</button>
+    `;
+
+    document.getElementById("score").innerText = "";
+}
+
+function next() {
+    document.getElementById("score").innerText = "Следующий вопрос...";
+}
