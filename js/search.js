6b56d3a21ae90d6e6470c06d73d1a3fa40edbd9b
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-22 01:42:44 +0600

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,16 +1,7 @@
-document.getElementById("search").addEventListener("input",(e)=>{
-
-const v = e.target.value.toLowerCase();
-
-const filtered = movies.filter(m=>
-m.title.toLowerCase().includes(v)
-);
-
-document.getElementById("row-trending").innerHTML =
-filtered.map(m=>`
-<div class="card" onclick="openMovie(${m.id})">
-<img src="${m.img}">
-<p>${m.title}</p>
-</div>
-`).join("");
+document.getElementById("searchInput").addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+  const filtered = movies.filter(m =>
+    m.title.toLowerCase().includes(value)
+  );
+  renderMovies(filtered);
 });
